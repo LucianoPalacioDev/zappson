04e9238012b6be7e4f84d571498622bb870685fe
LucianoPalacio <luciano.palacio.dev@gmail.com>
2025-06-19 19:43:35 -0300

    styles: improve the error fetch component

--- components/Episode/Error/index.tsx
+++ components/Episode/Error/index.tsx
@@ -1,4 +1,5 @@
 import { useLanguage } from "@/contexts/LanguageContext";
+import { useTheme } from "@/hooks/useThemeColor";
 import { Ionicons } from "@expo/vector-icons";
 import { Text, TouchableOpacity, View } from "react-native";
 import useStyles from "./styles";
@@ -13,22 +14,31 @@ export default function ErrorSeasonFetch({
   handleRetry: () => void;
 }) {
   const styles = useStyles();
+  const { colors } = useTheme();
   const { t } = useLanguage();
 
   return (
-    <View style={[styles.container, styles.errorContainer]}>
-      {/* TODO: use a common icon and simpson red */}
-      <Ionicons name="alert-circle" size={64} color="#ff4444" />
-      <Text style={styles.errorText}>{error}</Text>
-      <TouchableOpacity style={styles.buttonPrimary} onPress={handleRetry}>
-        <Text style={styles.buttonText}>{t("common.retry")}</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={[styles.buttonSecondary, { marginTop: 10 }]}
-        onPress={handleBack}
-      >
-        <Text style={styles.buttonText}>{t("common.backToHome")}</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <View style={styles.infoContainer}>
+        <Ionicons
+          name="alert-circle"
+          size={64}
+          color={colors.simpsonRed}
+          style={styles.errorIcon}
+        />
+        <Text style={styles.errorText}>{error}</Text>
+        <View style={styles.buttonsContainer}>
+          <TouchableOpacity style={styles.buttonPrimary} onPress={handleRetry}>
+            <Text style={styles.buttonText}>{t("common.retry")}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.buttonSecondary, { marginTop: 10 }]}
+            onPress={handleBack}
+          >
+            <Text style={styles.buttonText}>{t("common.backToHome")}</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
--- components/Episode/Error/styles.ts
+++ components/Episode/Error/styles.ts
@@ -5,18 +5,51 @@ export default function useStyles() {
   const { colors } = useTheme();
 
   return StyleSheet.create({
-    errorContainer: {
+    container: {
       flex: 1,
+      backgroundColor: colors.transparent,
       justifyContent: "center",
       alignItems: "center",
       padding: 20,
-      backgroundColor: colors.white,
     },
     errorText: {
       fontSize: 16,
       textAlign: "center",
       marginVertical: 20,
       color: colors.black,
+    },
+    infoContainer: {
+      width: "100%",
+      padding: 20,
+      backgroundColor: colors.white,
+      borderRadius: 12,
+      justifyContent: "space-between",
+    },
+    errorIcon: {
+      textAlign: "center",
+    },
+    buttonsContainer: {
+      width: "100%",
+      display: "flex",
+      flexDirection: "column",
+      gap: 12,
+    },
+    buttonPrimary: {
+      backgroundColor: colors.simpsonBlue,
+      padding: 16,
+      borderRadius: 16,
+      alignItems: "center",
+    },
+    buttonSecondary: {
+      backgroundColor: "#666",
+      padding: 16,
+      borderRadius: 16,
+      alignItems: "center",
+    },
+    buttonText: {
+      color: colors.white,
+      fontWeight: "bold",
+      fontSize: 16,
     },
   });
 }
